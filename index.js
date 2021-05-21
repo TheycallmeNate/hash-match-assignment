012e43353b149c6f6957a10e03a8efc8d4313b5e
TheycallmeNate <nathanmangal@gmail.com>
2021-05-21 19:09:38 -0400

    Completed 2. Create a hash from the text ReskillAmericans123 and save this as a variable.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const bcrypt = require("bcrypt");
 const app = require("express")();
+
+const saltRounds = 10;
+const myPlaintextPassword = "ReskillAmericans123";
+const someOtherPlaintextPassword = "not_bacon";
+let hashed = null;
+
+bcrypt.hash(myPlaintextPassword, saltRounds, (error, hash) => {
+  if (error) console.log(error);
+  hashed = hash;
+});
 
 app.listen(5000, () => console.log("server up"));
